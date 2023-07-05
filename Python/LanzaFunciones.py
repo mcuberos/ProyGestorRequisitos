8e8250899497a9b6bea9abd2b21bac2e6d3f534f
Manu Cuberos <mcuberos@hispacold.es>
2023-07-05 12:30:14 +0200

    FICHERO PARA EJECUTAR FUNCIONES

---
+++ Python/LanzaFunciones.py
@@ -0,0 +1,11 @@
+from tkinter import messagebox
+action=input("Choose C to create a new database. Choose L to load a CBC file to a existing database")
+
+if action=="C":
+    import createDatabase
+
+elif action=="L":
+    import LoadDatabase
+
+else:
+    messagebox.showinfo("WARNING!","AN INVALID OPTION HAS BEEN SELECTED. PLEASE RUN THE PROGRAM AGAIN.")
