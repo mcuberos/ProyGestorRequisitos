b76c0037544980d6fc96fc7d4b9572b81b53be8d
Manu Cuberos <mcuberos@hispacold.es>
2023-07-14 08:54:18 +0200

    Update LanzaFunciones para llamar nuevas funciones

--- Python/LanzaFunciones.py
+++ Python/LanzaFunciones.py
@@ -1,5 +1,5 @@
 from tkinter import messagebox
-action=input("Seleccione una opción (inserte solo el número): \n1. Crear una Base de Datos. \n2. Cargar un CBC a la Base de Datos. \n3. Cargar un fichero temporal a la base de datos \n")
+action=input("Seleccione una opción (inserte solo el número): \n1. Crear una Base de Datos. \n2. Cargar un CBC a la Base de Datos. \n3. Cargar un fichero temporal a la base de datos \n4. Cargar un CBC para generar un excel temporal con la propuesta de autorrellenado. \n5. Cargar un excel temporal para autorellenar un CbC \n")
 
 if action=="1":
     import createDatabase
@@ -9,6 +9,12 @@ elif action=="2":
 
 elif action=="3":
     import LoadDatabaseFromTemp
+
+elif action=="4":
+    import CreateCBCTemp
+
+elif action=="5":
+    import AutoFillCBCTemp
 
 else:
     messagebox.showinfo("WARNING!","AN INVALID OPTION HAS BEEN SELECTED. PLEASE RUN THE PROGRAM AGAIN.")
