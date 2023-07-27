6a39a9ff48db1bed405fb12009ac25aa02f295b6
Manu Cuberos <mcuberos@hispacold.es>
2023-07-27 14:36:10 +0200

    No se muestra la contraseña de admin en pantalla

--- Python/LanzaFunciones.py
+++ Python/LanzaFunciones.py
@@ -1,22 +1,24 @@
 from tkinter import messagebox
+import getpass
+
 action=input("Seleccione una opción (inserte solo el número): \n1. Crear una Base de Datos (SOLO ADMIN). \n2. Cargar un CBC a la Base de Datos (SOLO ADMIN). \n3. Cargar un fichero temporal a la base de datos (SOLO ADMIN). \n4. Cargar un CBC para generar un excel temporal con la propuesta de autorrellenado. \n5. Cargar un excel temporal para autorellenar un CbC \n")
 
 if action=="1":
-    pwd=input("INTRODUZCA LA CONTRASEÑA: ")
+    pwd=getpass.getpass("INTRODUZCA LA CONTRASEÑA: ")
     if pwd=="MCG@Trenes":
         import createDatabase
     else:
         messagebox.showinfo("Acceso Denegado","La contraseña introducida es incorrecta.")
 
 elif action=="2":
-    pwd=input("INTRODUZCA LA CONTRASEÑA: ")
+    pwd=getpass.getpass("INTRODUZCA LA CONTRASEÑA: ")
     if pwd=="MCG@Trenes":
         import LoadDatabase
     else:
         messagebox.showinfo("Acceso Denegado","La contraseña introducida es incorrecta.")
 
 elif action=="3":
-    pwd=input("INTRODUZCA LA CONTRASEÑA: ")
+    pwd=getpass.getpass("INTRODUZCA LA CONTRASEÑA: ")
     if pwd=="MCG@Trenes":
         import LoadDatabaseFromTemp
     else:
